e4b1b880ab4b83fbba4d1f07c82f64090dc73587
Wolfy2604 <tkachik.v.v@mail.ru>
2022-02-09 10:58:17 +0400

    3

--- m2m-relations/articles/admin.py
+++ m2m-relations/articles/admin.py
@@ -1,10 +1,34 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
 from .models import Article, TagPosition, Tag
 
 
+class TagPositionInlineFormset(BaseInlineFormSet):
+
+    def clean(self):
+        one_main = False
+        for form in self.forms:
+            try:
+                print(form.cleaned_data['is_main'])
+                if form.cleaned_data['is_main']:
+                    if one_main:
+                        raise ValidationError('Два главных тега')
+                    else:
+                        one_main = True
+                else:
+                    continue
+            except KeyError:
+                continue
+        if not one_main:
+            raise ValidationError('Нет главных тегов!')
+        return super().clean()
+
+
 class TagPositionInline(admin.TabularInline):
     model = TagPosition
+    formset = TagPositionInlineFormset
 
 
 @admin.register(Article)
